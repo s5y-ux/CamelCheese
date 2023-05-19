76d643c617a2f22d8ee8ecfa304cf6e8d6180c77
Joseph Shumaker <59636597+s5y-ux@users.noreply.github.com>
2023-05-18 23:25:36 -0700

    Add files via upload

---
+++ main.pl
@@ -0,0 +1,19 @@
+#!/usr/local/bin/perl
+
+#Function Body
+
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+
+sub copy_commit_delete{
+	for($index = 0; $index < 1 + int rand(4); $index++){
+		system("touch test.txt");
+		system("git add test.txt");
+		system("git commit -m Cheesed...");
+		system("git push")
+		system("git rm text.txt");
+		system("rm test.txt");
+	}
+}
+#Main Body
+
+copy_commit_delete();
